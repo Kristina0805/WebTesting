3f2170f6de0f6b48c96594283008b2d83ed49e5d
Kristina0805 <kristya25.95@mail.ru>
2022-11-03 10:36:24 +0500

    new test3

--- src/test/java/ru/netology/RequestTest.java
+++ src/test/java/ru/netology/RequestTest.java
@@ -40,7 +40,7 @@ public class RequestTest {
     }
 
     @Test
-    void test() {
+    void CorrectFillingTest() {
         driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Трапезникова Кристина");
@@ -51,5 +51,32 @@ public class RequestTest {
         Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
     }
+
+    @Test
+    void WrongNumberTest(){
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Трапезникова Кристина");
+        elements.get(1).sendKeys("79227280798");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        Assertions.assertEquals ("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+        //String text = driver.findElements(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+    }
+
+    @Test
+    void InvalidNameTest(){
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Trapeznikova Kristina");
+        elements.get(1).sendKeys("+79227280798");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        Assertions.assertEquals ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+
+    }
+
 
 }
